9382cffed3fdff175bae672bb220b313947b45af
Jason Gun <gunjc1@etsu.edu>
2026-02-01 20:24:22 -0500

    1st UI changes.

--- StockBuddy/Form1.Designer.cs
+++ StockBuddy/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace StockBuddy
 {
-    partial class Form1
+    partial class Home
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.CheckoutBtn = new System.Windows.Forms.Button();
+            this.InventoryBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CheckoutBtn
+            // 
+            this.CheckoutBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CheckoutBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(246)))), ((int)(((byte)(248)))));
+            this.CheckoutBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckoutBtn.Location = new System.Drawing.Point(169, 351);
+            this.CheckoutBtn.Name = "CheckoutBtn";
+            this.CheckoutBtn.Size = new System.Drawing.Size(216, 120);
+            this.CheckoutBtn.TabIndex = 0;
+            this.CheckoutBtn.Text = "Checkout";
+            this.CheckoutBtn.UseVisualStyleBackColor = false;
+            this.CheckoutBtn.Click += new System.EventHandler(this.CheckoutBtn_Click);
+            // 
+            // InventoryBtn
+            // 
+            this.InventoryBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.InventoryBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(246)))), ((int)(((byte)(248)))));
+            this.InventoryBtn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InventoryBtn.Location = new System.Drawing.Point(549, 347);
+            this.InventoryBtn.Name = "InventoryBtn";
+            this.InventoryBtn.Size = new System.Drawing.Size(219, 128);
+            this.InventoryBtn.TabIndex = 1;
+            this.InventoryBtn.Text = "Manage Inventory";
+            this.InventoryBtn.UseVisualStyleBackColor = false;
+            this.InventoryBtn.Click += new System.EventHandler(this.InventoryBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(169, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(635, 344);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(58)))), ((int)(((byte)(95)))));
+            this.ClientSize = new System.Drawing.Size(975, 581);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.InventoryBtn);
+            this.Controls.Add(this.CheckoutBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CheckoutBtn;
+        private System.Windows.Forms.Button InventoryBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- StockBuddy/Form1.cs
+++ StockBuddy/Form1.cs
@@ -10,11 +10,27 @@ using System.Windows.Forms;
 
 namespace StockBuddy
 {
-    public partial class Form1 : Form
+    public partial class Home : Form
     {
-        public Form1()
+        public Home()
         {
             InitializeComponent();
+        }
+
+        private void CheckoutBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InventoryBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            InventoryBtn.BringToFront();
+            CheckoutBtn.BringToFront();
         }
     }
 }
